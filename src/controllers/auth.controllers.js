847084241388b6a7ec0ac73e7235e311595221c2
Phebueno <fcbgomes@gmail.com>
2023-05-24 21:17:40 -0300

    feat: endpoints de signup e signin do usuário

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import { getUserByEmailDB, newSessionDB, newUserDB } from "../repositories/auth.repository.js";
+
+export async function signup(req, res) {
+  const { password } = req.body;
+  const passwordHash = bcrypt.hashSync(password, 10);
+  try {
+    const addUser = await newUserDB(req.body, passwordHash);
+    if (!addUser.rowCount) return res.status(409).send("Email já cadastrado!");
+    res.status(201).send("Usuário cadastrado!");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function signin(req, res) {
+  const { password } = req.body;
+  try {
+    const login = await getUserByEmailDB(req.body);
+
+    if (!login.rowCount) return res.status(401).send("Email não cadastrado.");
+    if (!bcrypt.compareSync(password, login.rows[0].password))
+      return res.status(401).send("Senha incorreta.");
+
+    //Criação de sessão
+    const token = uuid();
+    await newSessionDB(token, login);
+    res.send({ username: login.rows[0].username, token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
---
+++ src/repositories/auth.repository.js
@@ -0,0 +1,26 @@
+import { db } from "../database/database.connection.js";
+
+export function newUserDB(body, passwordHash) {
+  const { username, email, imgProfile, bio } = body;
+  return db.query(
+    `INSERT INTO users (username,email,password, "imgProfile", bio, "createdAt")
+              VALUES ($1,$2,$3,$4,$5,NOW())
+              ON CONFLICT DO NOTHING`,
+    [username, email, passwordHash, imgProfile, bio]
+  );
+}
+
+export function getUserByEmailDB(body) {
+    const { email } = body;
+    return db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+  }
+  
+  export function newSessionDB(token,login){
+      return db.query(
+          `INSERT INTO sessions ("userId",token,"createdAt")
+                VALUES ($1,$2,NOW())`,
+          [login.rows[0].id, token]
+        );
+  }
+  
+  

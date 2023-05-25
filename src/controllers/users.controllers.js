6c28b0582eac103dfddc2c67d68f1b0ea45d85f8
Phebueno <fcbgomes@gmail.com>
2023-05-25 17:21:01 -0300

    feat: endpoint de recuperar follows e followers

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 export async function getUserProfile(req,res){
     const {id} = req.params;
     try {
-        const userData = await db.query('SELECT username,"imgProfile",bio,"createdAt" FROM users WHERE id=$1',[id]);
+        const userData = await db.query('SELECT id, username,"imgProfile",bio,"createdAt" FROM users WHERE id=$1',[id]);
         //JUNTAR AOS POSTS!
         res.send(userData.rows[0]);
     } catch (err) {
--- src/routes/follows.routes.js
+++ src/routes/follows.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { followUser, unfollowUser } from "../controllers/follows.controllers.js";
+import { followUser, getFollowers, getFollows, unfollowUser } from "../controllers/follows.controllers.js";
 
 const followsRouter = Router();
+
+followsRouter.get("/users/:userId/followers", getFollowers);
+followsRouter.get("/users/:userId/follows", getFollows);
 followsRouter.post("/users/:userId/follow", followUser);
 followsRouter.delete("/users/:userId/unfollow", unfollowUser);
 

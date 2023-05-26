9536096640af66e530f3f91996141b74351baff9
Phebueno <fcbgomes@gmail.com>
2023-05-26 20:52:38 -0300

    feat: agora o nome da conta é informado pelas rotas de seguidores

--- src/controllers/follows.controllers.js
+++ src/controllers/follows.controllers.js
@@ -1,6 +1,11 @@
 import { db } from "../database/database.connection.js";
 import { getUserByIdDB } from "../repositories/auth.repository.js";
-import { deleteFollowDB, getFollowersByIdDB, getFollowsByIdDB, insertFollowDB } from "../repositories/follows.repository.js";
+import {
+  deleteFollowDB,
+  getFollowersByIdDB,
+  getFollowsByIdDB,
+  insertFollowDB,
+} from "../repositories/follows.repository.js";
 
 export async function followUser(req, res) {
   const followerId = res.locals.user.id;
@@ -8,7 +13,7 @@ export async function followUser(req, res) {
   if (Number(followerId) === Number(accountId))
     return res.status(409).send("Você não pode seguir a si mesmo!");
   try {
-    const follow = await insertFollowDB(followerId,accountId);
+    const follow = await insertFollowDB(followerId, accountId);
     if (!follow.rowCount)
       return res.status(404).send("Usuário não encontrado!");
     res.status(201).send("Seguindo o usuário!");
@@ -19,19 +24,20 @@ export async function followUser(req, res) {
   }
 }
 
-export async function unfollowUser(req, res) {  
+export async function unfollowUser(req, res) {
   const followerId = res.locals.user.id;
   const accountId = req.params.userId;
   if (Number(followerId) === Number(accountId))
     return res.status(409).send("Você não pode deixar de seguir a si mesmo!");
   try {
-    const unfollow = await deleteFollowDB(followerId,accountId)
-    if (!unfollow.rowCount){
+    const unfollow = await deleteFollowDB(followerId, accountId);
+    if (!unfollow.rowCount) {
       const user = await getUserByIdDB(accountId);
-      if(!user.rowCount) return res.status(404).send("Usuário não encontrado!"); 
+      if (!user.rowCount)
+        return res.status(404).send("Usuário não encontrado!");
       return res.status(409).send("Vocẽ não segue esse usuário!");
     }
-      
+
     res.status(204).send("Deixando de seguir o usuário!");
   } catch (err) {
     res.status(500).send(err.message);
@@ -41,29 +47,30 @@ export async function unfollowUser(req, res) {
 export async function getFollowers(req, res) {
   const accountId = req.params.userId;
   try {
-    const followers = await getFollowersByIdDB(req.params)
-    if (!followers.rowCount) {
-      //verifica se usuário é valido (pode virar middleware)
-      const user = await getUserByIdDB(accountId);
-      if (!user.rowCount) return res.status(404).send("Usuário não encontrado!");
-    }
-    res.status(200).send(followers.rows);
+    const user = await getUserByIdDB(accountId);
+    if (!user.rowCount) return res.status(404).send("Usuário não encontrado!");
+    const followers = await getFollowersByIdDB(req.params);
+    res.status(200).send(
+      {
+        username: user.rows[0].username, 
+        followers: followers.rows 
+      });
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function getFollows(req, res) {
-    const accountId = req.params.userId;
-    try {
-      const followers = await getFollowsByIdDB(req.params);
-      if (!followers.rowCount) {
-        //verifica se usuário é valido (pode virar middleware)
-        const user = await getUserByIdDB(accountId);
-        if (!user.rowCount) return res.status(404).send("Usuário não encontrado!");
-      }
-      res.status(200).send(followers.rows);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+  const accountId = req.params.userId;
+  try {
+    const user = await getUserByIdDB(accountId);
+    if (!user.rowCount) return res.status(404).send("Usuário não encontrado!");
+    const follows = await getFollowsByIdDB(req.params);
+    res.status(200).send({
+      username: user.rows[0].username, 
+      follows: follows.rows 
+    });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
